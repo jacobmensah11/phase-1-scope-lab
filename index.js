ecba862a1bdc7fdcbe7c3c2b44064ac871fe4734
jacobmensah11 <jacob.mensah@student.moringaschool.com>
2022-05-22 22:55:58 +0000

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+var customerName = 'bob';
+const leastFavoriteCustomer = 'malik';
+
+
+function upperCaseCustomerName(){
+
+  return customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+  bestCustomer = 'not bob';
+  
+}
+
+function overwriteBestCustomer(){
+  bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = 'meera';
+}
+
+console.log(changeLeastFavoriteCustomer());
+
 // Write your solution in this file!
